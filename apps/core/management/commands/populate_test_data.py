e18029fcc5cb8554e04c6e5f7620f632f0e043ed
DrShiny <doctor@drshiny.com>
2025-08-31 16:38:47 -0700

    updated test data set, perhaps the migrations all work now

--- apps/core/management/commands/populate_test_data.py
+++ apps/core/management/commands/populate_test_data.py
@@ -1,7 +1,10 @@
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
+from django.contrib.auth import get_user_model
 from apps.contacts.models import Contact
 from apps.jobs.models import Job
+
+User = get_user_model()
 
 
 class Command(BaseCommand):
@@ -19,7 +22,27 @@ class Command(BaseCommand):
             self.stdout.write('Clearing existing data...')
             Job.objects.all().delete()
             Contact.objects.all().delete()
+            User.objects.filter(username='dev_user').delete()
             self.stdout.write(self.style.SUCCESS('Existing data cleared.'))
+
+        # Create default development user
+        self.stdout.write('Creating default development user...')
+        default_user, created = User.objects.get_or_create(
+            username='dev_user',
+            defaults={
+                'email': 'dev@example.com',
+                'first_name': 'Dev',
+                'last_name': 'User',
+                'is_staff': True,
+                'is_superuser': True,
+            }
+        )
+        if created:
+            default_user.set_password('dev_password')
+            default_user.save()
+            self.stdout.write(self.style.SUCCESS('Default user created.'))
+        else:
+            self.stdout.write('Default user already exists.')
 
         self.stdout.write('Loading webserver test data from fixtures...')
         try:
