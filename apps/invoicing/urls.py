e58a640aa176318aca00af75d967640f1e0474ec
DrShiny <doctor@drshiny.com>
2025-09-25 22:06:23 -0700

    added price list to settings menu, updated views to not have fanciness

--- apps/invoicing/urls.py
+++ apps/invoicing/urls.py
@@ -6,4 +6,9 @@ app_name = 'invoicing'
 urlpatterns = [
     path('', views.invoice_list, name='invoice_list'),
     path('<int:invoice_id>/', views.invoice_detail, name='invoice_detail'),
+
+    # Price List Item URLs
+    path('price-list-items/', views.price_list_item_list, name='price_list_item_list'),
+    path('price-list-items/add/', views.price_list_item_add, name='price_list_item_add'),
+    path('price-list-items/<int:item_id>/edit/', views.price_list_item_edit, name='price_list_item_edit'),
 ]
--- apps/invoicing/views.py
+++ apps/invoicing/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Invoice, InvoiceLineItem
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
+from .models import Invoice, InvoiceLineItem, PriceListItem
+from .forms import PriceListItemForm
 
 def invoice_list(request):
     invoices = Invoice.objects.all().order_by('-invoice_id')
@@ -14,4 +16,51 @@ def invoice_detail(request, invoice_id):
         'invoice': invoice,
         'line_items': line_items,
         'total_amount': total_amount
+    })
+
+
+def price_list_item_list(request):
+    """Display all price list items."""
+    items = PriceListItem.objects.all().order_by('code')
+    return render(request, 'invoicing/price_list_item_list.html', {
+        'items': items
+    })
+
+
+def price_list_item_add(request):
+    """Add a new price list item."""
+    if request.method == 'POST':
+        form = PriceListItemForm(request.POST)
+        if form.is_valid():
+            item = form.save()
+            messages.success(request, f'Price List Item "{item.code}" created successfully.')
+            return redirect('invoicing:price_list_item_list')
+    else:
+        form = PriceListItemForm()
+
+    return render(request, 'invoicing/price_list_item_form.html', {
+        'form': form,
+        'title': 'Add Price List Item',
+        'button_text': 'Create Item'
+    })
+
+
+def price_list_item_edit(request, item_id):
+    """Edit an existing price list item."""
+    item = get_object_or_404(PriceListItem, price_list_item_id=item_id)
+
+    if request.method == 'POST':
+        form = PriceListItemForm(request.POST, instance=item)
+        if form.is_valid():
+            item = form.save()
+            messages.success(request, f'Price List Item "{item.code}" updated successfully.')
+            return redirect('invoicing:price_list_item_list')
+    else:
+        form = PriceListItemForm(instance=item)
+
+    return render(request, 'invoicing/price_list_item_form.html', {
+        'form': form,
+        'item': item,
+        'title': f'Edit Price List Item: {item.code}',
+        'button_text': 'Update Item'
     })
